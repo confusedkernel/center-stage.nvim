3207bb09cc8a4e931362f80d532fd711e66eddcc
teewhy <tyleryeh047@gmail.com>
2024-06-16 01:04:47 +0800

    mod(lua): StyLua - updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # center-stage.nvim
 
-A Neovim plugin that emulates the typewriter experience, by keeping the cursor in the center of your screen.
+A Neovim plugin that keeps your cursor at the center stage 24/7 (if you enable it, of course.)
+
+## Installation
+
+Using Lazy:
+```Lua
+{ "nottyl/center-stage.nvim" }
+```
+
+Using Packer:
+```Lua
+use "nottyl/center-stage.nvim"
+```
+
+## Options
+
+Coming soon!
+
+## Enabling Keymap
+
+`center-stage.nvim` can be used with function such as
+- `CCEnable`: Enable center-stage.nvim
+- `CCDisable`: Disable center-stage.nvim
+- `CCToggle`: Toggle center-stage.nvim on and off
+
+You can add a keymap to toggle the plugin more easily, example:
+```Lua
+vim.keymap.set({'n', 'v'}, '<leader>cc', require("center-stage").cc_toggle, { desc = "CCToggle" })
+```
--- lua/center-stage/init.lua
+++ lua/center-stage/init.lua
@@ -12,19 +12,19 @@ function M.center_cursor()
 end
 
 function M.cc_enable()
-    center_stage = true
-    api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
-        pattern = "*",
-        callback = M.center_cursor,
-        group = api.nvim_create_augroup("CenterStage", { clear = true }),
-    })
-    print("Center stage enabled.")
+	center_stage = true
+	api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+		pattern = "*",
+		callback = M.center_cursor,
+		group = api.nvim_create_augroup("CenterStage", { clear = true }),
+	})
+	print("Center stage enabled.")
 end
 
 function M.cc_disable()
-    center_stage = false
-    api.nvim_clear_autocmds({ group = "CenterStage" })
-    print("Center stage disabled.")
+	center_stage = false
+	api.nvim_clear_autocmds({ group = "CenterStage" })
+	print("Center stage disabled.")
 end
 
 function M.cc_toggle()
@@ -36,9 +36,9 @@ function M.cc_toggle()
 end
 
 function M.setup()
-    api.nvim_create_user_command("CcEnable", M.cc_enable, {})
-    api.nvim_create_user_command("CcDisable", M.cc_disable, {})
-    api.nvim_create_user_command("CcToggle", M.cc_toggle, {})
+	api.nvim_create_user_command("CCEnable", M.cc_enable, {})
+	api.nvim_create_user_command("CCDisable", M.cc_disable, {})
+	api.nvim_create_user_command("CCToggle", M.cc_toggle, {})
 end
 
 return M
